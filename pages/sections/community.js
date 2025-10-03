1f77e8674c9f22e7dff31f0978d1b25bc9e7d7f4
bakralq <bakralqq@gmail.com>
2025-10-02 19:57:08 -0500

    Delete pages directory

--- pages/sections/community.js
+++
@@ -1,3 +0,0 @@
-
-import Layout from '@/components/Layout';
-export default function Community(){return (<Layout><h1 className="font-serif text-3xl">Community — Coming Soon</h1></Layout>)}
--- pages/sections/governance.js
+++
@@ -1,3 +0,0 @@
-
-import Layout from '@/components/Layout';
-export default function Governance(){return (<Layout><h1 className="font-serif text-3xl">Governance — Coming Soon</h1></Layout>)}
